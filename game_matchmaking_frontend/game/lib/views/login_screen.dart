1dfca4cb2f8f1fb36931e76a601f405c6eb76b2a
LakshRJain <jainlaksh2006@gmail.com>
2025-10-30 20:53:28 +0530

    UI changes in login and signup page

--- game_matchmaking_frontend/game/lib/views/login_screen.dart
+++ game_matchmaking_frontend/game/lib/views/login_screen.dart
@@ -21,58 +21,81 @@ class _LoginScreenState extends State<LoginScreen> {
     final authVM = Provider.of<AuthViewModel>(context);
 
 
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
-      ),
-      body:Padding(
-        padding: const EdgeInsets.all(20),
-        child:Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              TextFormField(
-                controller: _emailCtrl,
-                decoration: const InputDecoration(
-                  labelText: 'Email',
+    return Container(
+      decoration: BoxDecoration(
+          gradient:  LinearGradient(colors: [Colors.white,Colors.blue],begin: Alignment.topCenter,end: Alignment.bottomCenter)
+        ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+        backgroundColor: Colors.transparent,
+          title: const Text('Login'),
+        ),
+        body:Padding(
+          padding: const EdgeInsets.all(30),
+          child:Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                TextFormField(
+                  controller: _emailCtrl,
+                  decoration: const InputDecoration(
+                    labelText: 'Email',
+                  ),
+                  validator: (val)=>val!.isEmpty?'Please enter your mail':null,
                 ),
-                validator: (val)=>val!.isEmpty?'Please enter your mail':null,
-              ),
-              const SizedBox(height: 20),
-              TextFormField(
-                controller: _passCtrl,
-                decoration: const InputDecoration(
-                  labelText: 'Password'
+                const SizedBox(height: 20),
+                TextFormField(
+                  controller: _passCtrl,
+                  decoration: const InputDecoration(
+                    labelText: 'Password'
+                  ),
+                  validator: (val)=>val!.isEmpty?'Please enter your password':null,
                 ),
-                validator: (val)=>val!.isEmpty?'Please enter your password':null,
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: authVM.isLoading?null:()async{
-                  if(_formKey.currentState!.validate()){
-                    await authVM.login(_emailCtrl.text, _passCtrl.text);
-                    if(authVM.user!=null &&mounted){
-                      Navigator.popAndPushNamed(context, '/profile');
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    fixedSize: Size(150, 50),
+                    backgroundColor: Colors.transparent,
+                    foregroundColor: Colors.black,
+                    elevation: 0,
+                    shape: RoundedRectangleBorder(
+                      
+                      borderRadius: BorderRadius.circular(40),
+                      side: BorderSide(color: Colors.black,width: 1.5)
+                    )
+                  ),
+                  onPressed: authVM.isLoading?null:()async{
+                    if(_formKey.currentState!.validate()){
+                      await authVM.login(_emailCtrl.text, _passCtrl.text);
+                      if(authVM.user!=null &&mounted){
+                        Navigator.popAndPushNamed(context, '/profile');
+                      }
+                      else{
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text('Login failed'))
+                        );
+                      }
                     }
-                    else{
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(content: Text('Login failed'))
-                      );
-                    }
-                  }
-                },
-                child: authVM.isLoading?
-                const CircularProgressIndicator():
-                const Text('Login')
-              ),
-              const SizedBox(height: 20,),
-              TextButton(onPressed: (){
-                Navigator.popAndPushNamed(context, '/signup');
-              }, child: Text('SignUp'))
-            ],
-          ),
-        )
+                  },
+                  child: authVM.isLoading?
+                  const CircularProgressIndicator():
+                  const Text('Login')
+                ),
+                const SizedBox(height: 20,),
+                TextButton(onPressed: (){
+                  Navigator.popAndPushNamed(context, '/signup');
+                }, 
+                child: Text('SignUp'),
+                style: TextButton.styleFrom(
+                  foregroundColor: Colors.black
+                ),
+                )
+              ],
+            ),
+          )
+        ),
       ),
     );
   }
--- game_matchmaking_frontend/game/lib/views/signup_screen.dart
+++ game_matchmaking_frontend/game/lib/views/signup_screen.dart
@@ -21,64 +21,85 @@ class _SignupScreenState extends State<SignupScreen> {
   @override
   Widget build(BuildContext context) {
     final authVM = Provider.of<AuthViewModel>(context);
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Sign Up'),
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(colors: [Colors.white,Colors.blue],begin:Alignment.topCenter , end: Alignment.bottomCenter)
+      
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              TextFormField(
-                controller: _usernameCtrl,
-                decoration: const InputDecoration(
-                  labelText: 'username'
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          title: const Text('Sign Up'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(30),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                TextFormField(
+                  controller: _usernameCtrl,
+                  decoration: const InputDecoration(
+                    labelText: 'username'
+                  ),
+                  validator: (val)=>val!.isEmpty?'Please enter username':null,
                 ),
-                validator: (val)=>val!.isEmpty?'Please enter username':null,
-              ),
-              SizedBox(height: 20,),
-              TextFormField(
-                controller: _emailCtrl,
-                decoration: const InputDecoration(
-                  labelText: 'email'
+                SizedBox(height: 20,),
+                TextFormField(
+                  controller: _emailCtrl,
+                  decoration: const InputDecoration(
+                    labelText: 'email'
+                  ),
+                  validator: (val)=>val!.isEmpty?'Please enter email':null,
                 ),
-                validator: (val)=>val!.isEmpty?'Please enter email':null,
-              ),
-              SizedBox(height: 20,),
-              TextFormField(
-                controller: _passCtrl,
-                decoration: const InputDecoration(
-                  labelText: 'password'
+                SizedBox(height: 20,),
+                TextFormField(
+                  controller: _passCtrl,
+                  decoration: const InputDecoration(
+                    labelText: 'password'
+                  ),
+                  validator: (val)=>val!.isEmpty?'Please enter password':null,
                 ),
-                validator: (val)=>val!.isEmpty?'Please enter password':null,
-              ),
-              SizedBox(height: 20,),
-              ElevatedButton(
-                onPressed: authVM.isLoading?
-                null:
-                () async{
-                  if(_formKey.currentState!.validate()){
-                    await authVM.register(_usernameCtrl.text, _emailCtrl.text, _passCtrl.text);
-                    if(authVM.user!=null &&mounted){
-                      Navigator.popAndPushNamed(context, '/profile');
+                SizedBox(height: 20,),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.transparent,
+                    foregroundColor: Colors.black,
+                    fixedSize: Size(150, 50),
+                    elevation: 0,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(40),
+                      side: BorderSide(color: Colors.black)
+                    )
+                  ),
+                  onPressed: authVM.isLoading?
+                  null:
+                  () async{
+                    if(_formKey.currentState!.validate()){
+                      await authVM.register(_usernameCtrl.text, _emailCtrl.text, _passCtrl.text);
+                      if(authVM.user!=null &&mounted){
+                        Navigator.popAndPushNamed(context, '/profile');
+                      }
+                      else{
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text('Sign Up failed'))
+                        );
+                      }
                     }
-                    else{
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(content: Text('Sign Up failed'))
-                      );
-                    }
-                  }
-                },
-                child: authVM.isLoading?CircularProgressIndicator():Text('Sign Up')
-              ),
-              SizedBox(height: 20,),
-              TextButton(onPressed: (){
-                Navigator.popAndPushNamed(context, '/login');
-              }, child: const Text('Login')),
-            ],
+                  },
+                  child: authVM.isLoading?CircularProgressIndicator():Text('Sign Up')
+                ),
+                SizedBox(height: 20,),
+                TextButton(onPressed: (){
+                  Navigator.popAndPushNamed(context, '/login');
+                }, child: const Text('Login'),
+                style: TextButton.styleFrom(
+                  foregroundColor: Colors.black
+                ),),
+              ],
+            ),
           ),
         ),
       ),
